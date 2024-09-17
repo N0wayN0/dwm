569da30bdebfaee0424c491af20caa8c0b8a4deb
root <root@debian.debian12>
2024-09-17 08:15:39 -0400

    mouse wheel can swap windows

--- config.h
+++ config.h
@@ -126,17 +126,20 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	//{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkWinTitle,          0,              Button1,        focusstack,     {.i = +1 } },
-	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
-	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
-	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
+	{ ClkWinTitle,          0,              Button4,        focusstack,     {.i = +1 } },
+	{ ClkWinTitle,          0,              Button5,        focusstack,     {.i = -1 } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} }, // left mouse button
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} }, // middle mouse button
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} }, // right mouse button
+	{ ClkStatusText,        0,              Button4,        sigstatusbar,   {.i = 4} }, // mouse wheel up
+	{ ClkStatusText,        0,              Button5,        sigstatusbar,   {.i = 5} }, // mouse wheel down
     { ClkStatusText,        ShiftMask,      Button1,        sigstatusbar,   {.i = 6} },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            0,              Button2,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
